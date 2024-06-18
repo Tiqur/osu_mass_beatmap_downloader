231105b02e96a38e02e732ef13f4bd59712c53a2
Tiqur <trevorbrage02@gmail.com>
2024-06-18 10:54:49 -0700

    Handle page not found edgecase

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
   "regexp"
   "io"
   "time"
+  "errors"
+  "strings"
 )
 
 type GameMode string;
@@ -25,28 +27,41 @@ func get_all_ranked_beatmap_ids_of_gamemode(gm GameMode) {
   const uri = "https://osu.ppy.sh/beatmaps/packs/";
   for i := 1; true; i++ {
     var pack_url = uri + string(gm) + fmt.Sprint(i);
-    get_map_ids_from_pack_url(pack_url);
+
+    err := get_map_ids_from_pack_url(pack_url);
+    if err != nil {
+      log.Fatalln(err);
+      break;
+    }
+
     time.Sleep(4 * time.Second);
   }
 }
 
-func get_map_ids_from_pack_url(pack_url string) {
+func get_map_ids_from_pack_url(pack_url string) error {
   resp, err := http.Get(pack_url);
 
   if err != nil {
-     log.Fatalln(err)
+    return errors.New("Error sending get request.")
   }
 
   body, err := io.ReadAll(resp.Body)
   if err != nil {
-    log.Fatalln(err)
+    return errors.New("Error reading response body.")
+  }
+
+  body_string := string(body);
+
+  if strings.Contains(body_string, "Page Missing") {
+    return errors.New("Page not found.")
   }
 
   pattern := regexp.MustCompile(`https:\/\/osu\.ppy\.sh\/beatmapsets\/([^\/"]+)`);
-  matches := pattern.FindAllStringSubmatch(string(body), -1);
+  matches := pattern.FindAllStringSubmatch(body_string, -1);
 
   for _, match := range matches {
     fmt.Println("Captured segment:", match[0]);
   }
-
+  
+  return nil;
 }
